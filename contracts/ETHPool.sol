d844c3fb98501ecb58aa15da54d7e2d4c920b380
Gallbers <gagzu.ali@gmail.com>
2022-01-25 11:38:36 -0300

    nuevos test - implementación del método stake()

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Challenge ETHPool
@@ -10,20 +11,38 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @dev ETHPool provider a service where people can deposit ETH and they will receive weekly rewards
 /// @custom:challenge This is a smart contract challenge
 contract ETHPool is Ownable {
+	using Counters for Counters.Counter;
+	Counters.Counter private _poolIds;
+
 	/// @dev General information of a user
 	struct User {
+		uint[] myPools;
 		uint balanceDeposited;
-		uint initialdepositDate;
 		address payable beneficiary;
 	}
 
-	/// @dev Pool management team
+	/// @dev Pool to store the information of a certain group of users
+	struct Pool {
+		uint balance;
+		uint rewards;
+		/// @dev user address => user data
+		mapping (address => User) beneficiaries;
+	}
+
+	/// @dev pool id => pool data
+	mapping (uint => Pool) private _pools;
+
+	/// @dev any address => `true` if it belongs to the team
 	mapping (address => bool) private _teamMembers;
 
-	/*
-	 * Mapping para los usuarios
-	 * mapping (address => User) public users;
-	*/
+	/// @dev user address => user data
+	mapping (address => User) private _users;
+
+	/// @dev Throws if you call by any account that is not a team member
+	modifier onlyTeam {
+		require(_teamMembers[msg.sender] == true, "Caller is not a member of the team");
+		_;
+	}
 
 	constructor() {
 		_teamMembers[msg.sender] = true;
@@ -47,7 +66,46 @@ contract ETHPool is Ownable {
 	/**
 	 * @notice Allow users to deposit their ETH to receive rewards
 	*/
-	function stake() public payable {}
+	function stake(uint amount) public payable {
+		require(msg.value > 0, "The amount must be > 0");
+		require(amount == msg.value, "The amount does not match the balance sent");
+
+		uint currentPoolId = _poolIds.current();
+
+		/// @dev No pool created yet
+		if(currentPoolId == 0) {
+			_poolIds.increment();
+			_stake(_poolIds.current());
+		}
+
+		/// @dev The pool does not yet have a reward and you can continue to accumulate balance
+		if(currentPoolId > 0 && _pools[currentPoolId].rewards == 0) {
+			_stake(currentPoolId);
+		}
+
+		/// @dev The pool has already received a reward and a new pool must be created
+		if(currentPoolId > 0 && _pools[currentPoolId].rewards > 0) {
+			_poolIds.increment();
+			_stake(_poolIds.current());
+		}
+	}
+
+	function _stake(uint currentPoolId) private {
+		Pool storage p = _pools[currentPoolId];
+
+		p.balance += msg.value;
+
+		if(p.beneficiaries[msg.sender].balanceDeposited == 0) {
+			p.beneficiaries[msg.sender].myPools.push(currentPoolId);
+			p.beneficiaries[msg.sender].beneficiary = payable(msg.sender);
+		}
+
+		if(_users[msg.sender].beneficiary == address(0)) {
+			_users[msg.sender] = p.beneficiaries[msg.sender];
+		}
+
+		p.beneficiaries[msg.sender].balanceDeposited += msg.value;
+	}
 
 	/**
 	 * @notice Allow users to withdraw their deposit and reward if applicable
@@ -56,13 +114,8 @@ contract ETHPool is Ownable {
 
 	/*
 	 * Depositar recompensa
-	 * function depositReward() public payable onlyTeam {}
 	*/
-
-	/*
-	 * Modificador donde solo el equipo puede llamar a la fn
-	 * moifier onlyTeam { _; }
-	*/
+	function depositReward() public payable onlyTeam {}
 
 	receive() external payable {}
 
--- test/ETHPool.test.ts
+++ test/ETHPool.test.ts
@@ -1,6 +1,6 @@
 import { ETHPool } from '../build/types';
 import { expect } from './utils/chaiSetup';
-import { parseEther } from 'ethers/lib/utils';
+import { formatEther, parseEther } from 'ethers/lib/utils';
 import { setupUser } from '../helpers/ethers';
 import { Accounts } from '../typescript/hardhat';
 import { deployments, ethers, getNamedAccounts } from 'hardhat';
@@ -14,7 +14,10 @@ async function setup () {
 
   const accounts = (await getNamedAccounts()) as Accounts;
 
+  const deployer = await setupUser(accounts.deployer, contracts);
+
   return {
+    deployer,
     accounts,
     contracts,
   }
@@ -74,6 +77,49 @@ describe('ETHPool', () => {
     const { contracts, accounts } = await setup();
 
     await expect(contracts.ETHPool.setTeamMember(accounts.user1)).to.not.reverted;
-  })
+  });
+
+  describe('Evaluating stake logic', () => {
+    it('The amount must be > 0', async () => {
+      const { contracts } = await setup();
+
+      await expect(contracts.ETHPool.stake(0)).to.be.revertedWith('The amount must be > 0');
+    });
+
+    it('should fail if the amount does not match the sent balance', async () => {
+      const { contracts } = await setup();
+
+      await expect(
+        contracts.ETHPool.stake(10, { value: 100 })
+      ).to.be.revertedWith('The amount does not match the balance sent');
+    });
+
+    it('Contract balance correctly increases after staking', async () => {
+      const { contracts, accounts } = await setup();
+
+      const userConnectedContract = await setupUser(accounts.user1, contracts);
+
+      const balanceBeforeStaking = await ethers.provider.getBalance(contracts.ETHPool.address);
+
+      const { ETHPool } = userConnectedContract;
+
+      const balanceToStaking = parseEther('100');
+
+      await expect(() => {
+        return ETHPool.stake(balanceToStaking, {
+          value: balanceToStaking
+        })
+      }).to.be.changeEtherBalance(ETHPool, balanceToStaking)
+
+      const balanceAfterStaking = await ethers.provider.getBalance(contracts.ETHPool.address);
+
+      expect(balanceAfterStaking).to.equal(balanceBeforeStaking.add(balanceToStaking))
+    });
+
+    it('Users and pool balance should increment correctly', async () => {
+      // TODO: staking balance with users A and B and verify that the balances are increased correctly
+      throw new Error('not implemented');
+    });
+  });
 
 })
